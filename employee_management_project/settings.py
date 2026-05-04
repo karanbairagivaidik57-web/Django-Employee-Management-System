d8a53ebd3e0b14fa1094a34857f4d26f5a8939df
Karan Bairagi <karanbairagivaidik57@gmail.com>
2026-05-04 17:47:04 +0530

    updated ALLOWED_HOSTS

--- employee_management_project/settings.py
+++ employee_management_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=jzzvlqk#_dx&9fhe8xo$*vl1p&8t*o%yigquv7wlu#m&laoj2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
