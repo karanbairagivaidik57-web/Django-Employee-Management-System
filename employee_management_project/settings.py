96f7516d42548301e2f9dd47bf89f273a708d9e7
Karan Bairagi <karanbairagivaidik57@gmail.com>
2026-05-20 19:26:22 +0530

    Final Updated

--- employee_management_project/settings.py
+++ employee_management_project/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-import pymysql
-pymysql.install_as_MySQLdb()
 
 from pathlib import Path
 
